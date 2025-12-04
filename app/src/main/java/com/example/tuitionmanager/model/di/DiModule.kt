f16099b4ea9594560373764c4f970db4afee915c
Sourendu05 <subhaschandramandal73@gmail.com>
2025-12-05 01:24:25 +0530

    Initial Files Created

---
+++ app/src/main/java/com/example/tuitionmanager/model/di/DiModule.kt
@@ -0,0 +1,11 @@
+package com.example.tuitionmanager.model.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DiModule {
+
+}
